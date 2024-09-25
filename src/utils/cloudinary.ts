6fd6fad9b299f70d16532543fc437f3960cb1ae5
NayimWd <nayim.wd@gmail.com>
2024-09-25 07:40:54 +0600

    cloudinary and multer setup done

---
+++ src/utils/cloudinary.ts
@@ -0,0 +1,39 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUDE_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+//  upload function
+const uploadOnCloudinary = async (localFilePath: any) => {
+  try {
+    if (!localFilePath) return null;
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+      transformation: [
+        { width: 360, height: 360, crop: "limit" },
+        { quality: 90 },
+      ],
+    });
+    // after file upload
+    console.log("file uploaded", response.url);
+
+    // Remove the local file after successful upload
+    fs.unlinkSync(localFilePath);
+
+    return response;
+  } catch (error) {
+    // remove locally saved temp file after upload failed
+    if (fs.existsSync(localFilePath)) {
+      fs.unlinkSync(localFilePath);
+    }
+    fs.unlinkSync(localFilePath); 
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
