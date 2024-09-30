8c3ffe3d6334af83dcfadd8cbaf42096285f6472
NayimWd <nayim.wd@gmail.com>
2024-09-30 09:57:48 +0600

    centralized router setup added

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -1,15 +1,20 @@
-import multer from "multer";
+import multer, { StorageEngine } from "multer";
+import { Request } from "express";
+import { Callback } from "mongoose";
 // multer file upload
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-     
-      cb(null, file.originalname)
-    }
-  });
+const storage: StorageEngine = multer.diskStorage({
+  destination: function (
+    req: Request,
+    file: Express.Multer.File,
+    cb: Callback
+  ) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req: Request, file: Express.Multer.File, cb: Callback) {
+    cb(null, file.originalname);
+  },
+});
 
-  export const upload = multer({
-    storage
-  })
+export const upload = multer({
+  storage,
+});
